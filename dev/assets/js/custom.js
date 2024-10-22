ce5848a3d4835cf7c16bc19bbb3c5fbae7f913ff
RostSuslov <suslovros@gmail.com>
2024-10-22 21:23:04 +0300

    feat: footer done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,9 @@
-  
+const btnBurger = document.querySelector('.btn-burger')
+const mobileContainer = document.querySelector('.mobile-container')
+
+const toggleMobileMenu =()=> {
+    mobileContainer.classList.toggle('show')
+    btnBurger.classList.toggle('active')
+}
+ 
+btnBurger.addEventListener('click', toggleMobileMenu) 
